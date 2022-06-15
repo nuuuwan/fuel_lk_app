1c2cf7c90f045f645cb08b9bb21b6b4c8416759e
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-15 18:02:53 +0530

    Find and pass recent dispatches

--- src/view/molecules/FuelsView.js
+++ src/view/molecules/FuelsView.js
@@ -4,17 +4,33 @@ import { FUEL_IDX } from "../../nonview/core/Fuel";
 
 import FuelView from "../../view/molecules/FuelView";
 
+import TimeX, {SECONDS_IN} from "../../nonview/base/TimeX"
+
 export default function FuelsView({ shedStatus }) {
+  const currentTime = TimeX.getUnixTime();
   return (
     <List>
       {Object.entries(FUEL_IDX).map(function ([fuelId, fuel]) {
         const shedFuel = shedStatus["fuel_status_idx"][fuelId];
+
+        let recentDispatch = null;
+        for (let dispatch of shedStatus["dispatch_schedule_list"]) {
+          if (dispatch["fuel_type"] !== fuelId) {
+            continue;
+          }
+          const deltaToDispatch = dispatch["time_eta_ut"] - currentTime;
+          if (deltaToDispatch > -SECONDS_IN.HOUR * 6) {
+            recentDispatch = dispatch;
+          }
+        }
+
         return (
           <FuelView
             key={"fuel-" + fuelId}
             label={fuel.name}
             availlability={shedFuel["is_available"]}
             capacity={shedFuel.capacity}
+            recentDispatch={recentDispatch}
             color={fuel.color}
           />
         );
