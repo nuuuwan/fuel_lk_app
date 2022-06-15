e480d4c439229c9858f34fddf381b0b12ef7af03
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-15 09:49:36 +0530

    Use SriLankaColors and Nunito Sans

--- src/App.js
+++ src/App.js
@@ -3,18 +3,34 @@ import React, { Component } from "react";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 import HomePage from "./view/organisms/HomePage.js";
+import SriLankaColors from "./nonview/constants/SriLankaColors"
+
+const THEME = createTheme({
+  palette: {
+    primary: {
+      main: SriLankaColors.Sinhala,
+    },
+    secondary: {
+      main: SriLankaColors.Tamil,
+    },
+    success: {
+      main: SriLankaColors.Muslim,
+    },
+    info: {
+      main: SriLankaColors.Buddhist,
+    },
+  },
+  typography: {
+    fontFamily: ["Nunito Sans", "sans-serif"].join(","),
+    fontSize: 15,
+  },
+});
+
 
 export default class App extends Component {
   render() {
-    const theme = createTheme({
-      typography: {
-        fontFamily: ["PT Sans", "sans-serif"].join(","),
-        fontSize: 14,
-      },
-    });
-
     return (
-      <ThemeProvider theme={theme}>
+      <ThemeProvider theme={THEME}>
         <HomePage />
       </ThemeProvider>
     );
