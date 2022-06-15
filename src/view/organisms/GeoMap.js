088cdc2a15cc9b87e93853e79096097f279118ec
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-15 07:59:24 +0530

    Style

--- src/view/organisms/GeoMap.js
+++ src/view/organisms/GeoMap.js
@@ -9,14 +9,14 @@ const URL_FORMAT = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
 
 const STYLE_MAP = {
   zIndex: 0,
-  width: '100%',
-  height: '100vh',
-  position: 'fixed',
+  width: "100%",
+  height: "100vh",
+  position: "fixed",
   top: 0,
   left: 0,
   bottom: 0,
   right: 0,
-}
+};
 export default class GeoMap extends Component {
   render() {
     const { center, zoom } = this.props;
