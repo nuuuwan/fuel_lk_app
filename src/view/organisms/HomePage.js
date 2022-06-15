5b3e80ce39a37266448c4c34de8e9be854457962
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-15 07:59:58 +0530

    Added nShedsLoaded

--- src/view/organisms/HomePage.js
+++ src/view/organisms/HomePage.js
@@ -1,13 +1,14 @@
 import { Component } from "react";
 import * as React from "react";
 
+import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 
 import FuelLKAppServer from "../../nonview/core/FuelLKAppServer";
 import CustomAppBar from "../../view/molecules/CustomAppBar.js";
 import CustomBottomNavigation from "../../view/molecules/CustomBottomNavigation.js";
-import GeoMap from "../../view/organisms/GeoMap"
-import ShedView from "../../view/organisms/ShedView"
+import GeoMap from "../../view/organisms/GeoMap";
+import ShedView from "../../view/organisms/ShedView";
 
 const STYLE = {
   margin: 4,
@@ -21,7 +22,7 @@ const DEFAULT_ZOOM = 10;
 export default class HomePage extends Component {
   constructor(props) {
     super(props);
-    this.state = { shedBasics: null };
+    this.state = { shedBasics: [], loadedShedIdSet: new Set() };
   }
 
   async componentDidMount() {
@@ -31,20 +32,23 @@ export default class HomePage extends Component {
       district,
       fuelType
     );
-    const shedBasics = shedBasicsAll.filter(
-      function(shedBasic) {
-        return shedBasic.shedownerupdatetoday;
-      }
-    );
+    const shedBasics = shedBasicsAll.filter(function (shedBasic) {
+      return shedBasic.shedownerupdatetoday;
+    });
 
-    this.setState({ shedBasics });
+    this.setState({ shedBasics, loadedShedIdSet: new Set() });
   }
 
-  onClickBack() {
-    window.history.back();
+  onClickRefresh() {
+    localStorage.clear();
     window.location.reload(true);
   }
 
+  onLoad(shedID) {
+    let { loadedShedIdSet } = this.state;
+    loadedShedIdSet.add(shedID);
+    this.setState({ loadedShedIdSet: loadedShedIdSet });
+  }
 
   renderInner() {
     const { shedBasics } = this.state;
@@ -52,22 +56,35 @@ export default class HomePage extends Component {
       return null;
     }
     return shedBasics.map(
-      function(shedBasic, iShed) {
+      function (shedBasic, iShed) {
         return (
-          <ShedView key={"shed-" + iShed} shedBasic={shedBasic} />
+          <ShedView
+            key={"shed-" + iShed}
+            shedBasic={shedBasic}
+            onLoad={this.onLoad.bind(this)}
+          />
         );
-      }
+      }.bind(this)
     );
   }
 
   render() {
+    const { loadedShedIdSet, shedBasics } = this.state;
+    const nShedsLoaded = loadedShedIdSet.size;
+    const nShedsTotal = shedBasics.length;
+
     return (
       <Box sx={STYLE}>
-        <CustomAppBar />
+        <CustomAppBar nShedsLoaded={nShedsLoaded} />
+        <Typography>{nShedsLoaded}</Typography>
         <GeoMap center={DEFAULT_CENTER} zoom={DEFAULT_ZOOM}>
           {this.renderInner()}
         </GeoMap>
-        <CustomBottomNavigation onClickBack={this.onClickBack.bind(this)} />
+        <CustomBottomNavigation
+          onClickRefresh={this.onClickRefresh.bind(this)}
+          nShedsTotal={nShedsTotal}
+          nShedsLoaded={nShedsLoaded}
+        />
       </Box>
     );
   }
