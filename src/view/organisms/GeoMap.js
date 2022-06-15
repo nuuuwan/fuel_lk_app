c3aeae18f7ce66f533446fcf076de1b75fd369aa
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-15 23:33:00 +0530

    Smooth move on click

--- src/view/organisms/GeoMap.js
+++ src/view/organisms/GeoMap.js
@@ -1,5 +1,5 @@
 import { Component } from "react";
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, TileLayer, useMapEvent } from "react-leaflet";
 
 const URL_FORMAT = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
 
@@ -13,6 +13,16 @@ const STYLE_MAP = {
   bottom: 0,
   right: 0,
 };
+
+function SetViewOnClick() {
+  const map = useMapEvent("click", (e) => {
+    map.setView(e.latlng, map.getZoom(), {
+      animate: true,
+    });
+  });
+  return null;
+}
+
 export default class GeoMap extends Component {
   render() {
     const { center, zoom } = this.props;
@@ -25,6 +35,7 @@ export default class GeoMap extends Component {
       >
         <TileLayer url={URL_FORMAT} />
         {this.props.children}
+        <SetViewOnClick />
       </MapContainer>
     );
   }
