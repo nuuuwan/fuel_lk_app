38a4aacad89408c6074c1a144427422daad60648
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-15 15:19:10 +0530

    Added multigetShedStatusses

--- src/nonview/core/FuelData.js
+++ src/nonview/core/FuelData.js
@@ -1,5 +1,20 @@
-export default class FuelData {
-  static async multigetShedStatusses() {
+const URL_DATA_ROOT = 'https://raw.githubusercontent.com/nuuuwan/fuel_lk/data';
 
+const JSON_HEADERS = {
+  headers: {
+    Accept: "application/json",
+  },
+};
+
+async function jsonNonCache(url) {
+  const response = await fetch(url, JSON_HEADERS);
+  const dataJson = await response.json();
+  return dataJson;
+}
+
+export default class FuelData {
+  static async multigetShedStatusList() {
+    const url = URL_DATA_ROOT + '/latest/shed_status_list.all.json';
+    return await jsonNonCache(url);
   }
 }
