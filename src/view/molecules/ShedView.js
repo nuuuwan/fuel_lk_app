27bc0aecb0beaf2be37df461a326906d27c9d2bd
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-15 15:31:49 +0530

    Make ShedView a molecule

---
+++ src/view/molecules/ShedView.js
@@ -0,0 +1,30 @@
+import { CircleMarker, Popup } from "react-leaflet";
+
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+
+import FuelsView from "../../view/molecules/FuelsView";
+
+const DEFAULT_CIRLE_RADIUS = 10;
+
+export default function ShedView({ shedStatus }) {
+  return (
+    <CircleMarker
+      center={shedStatus["lat_lng"]}
+      radius={DEFAULT_CIRLE_RADIUS}
+      pathOptions={{ color: "red", stroke: null, fillOpacity: 0.5 }}
+    >
+      <Popup closeButton={false}>
+        <Box sx={{ maxHeight: "67vh", overflow: "scroll" }}>
+          <Typography variant="caption">
+            {"Last updated: " + shedStatus["time_last_updated"]}
+          </Typography>
+          <Typography variant="subtitle1">{shedStatus["shed_name"]}</Typography>
+          <Typography variant="caption">{shedStatus["address"]}</Typography>
+
+          <FuelsView shedStatus={shedStatus} />
+        </Box>
+      </Popup>
+    </CircleMarker>
+  );
+}
