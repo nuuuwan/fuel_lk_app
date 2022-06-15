1edc4f74476297a816af4df670edc49001b98e4e
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-15 15:27:22 +0530

    Use new data source

--- src/view/organisms/ShedView.js
+++ src/view/organisms/ShedView.js
@@ -11,38 +11,23 @@ import FuelsView from "../../view/molecules/FuelsView";
 const DEFAULT_CIRLE_RADIUS = 10;
 
 export default class ShedView extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { shed: null };
-  }
-
-  async componentDidMount() {
-    const { shedBasic, onLoad } = this.props;
-    const shed = await FuelLKAppServer.getShed(shedBasic.shedId);
-    onLoad(shed);
-    this.setState({ shed });
-  }
-
   render() {
-    const { shed } = this.state;
-    if (!shed) {
-      return null;
-    }
+    const { shedStatus } = this.props;
     return (
       <CircleMarker
-        center={shed.latLng}
+        center={shedStatus['lat_lng']}
         radius={DEFAULT_CIRLE_RADIUS}
         pathOptions={{ color: "red", stroke: null, fillOpacity: 0.5 }}
       >
         <Popup closeButton={false}>
           <Box sx={{ maxHeight: "67vh", overflow: "scroll" }}>
             <Typography variant="caption">
-              {"Last updated: " + shed.lastUpdateTimeDate.toLocaleString()}
+              {"Last updated: " + shedStatus['time_last_updated']}
             </Typography>
-            <Typography variant="subtitle1">{shed.shedName}</Typography>
-            <Typography variant="caption">{shed.address}</Typography>
+            <Typography variant="subtitle1">{shedStatus['shed_name']}</Typography>
+            <Typography variant="caption">{shedStatus['address']}</Typography>
 
-            <FuelsView shed={shed} />
+            <FuelsView shedStatus={shedStatus} />
           </Box>
         </Popup>
       </CircleMarker>
