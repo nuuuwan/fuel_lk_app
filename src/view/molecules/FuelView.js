1c80d5dc77acbd5f21c963958ddec14023dd2096
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-15 22:55:34 +0530

    Use new icons

--- src/view/molecules/FuelView.js
+++ src/view/molecules/FuelView.js
@@ -5,6 +5,10 @@ import FuelQuantity from "../../view/atoms/FuelQuantity";
 import HumanTime from "../../view/atoms/HumanTime";
 import Place from "../../view/atoms/Place";
 import LabelledBox from "../../view/molecules/LabelledBox";
+import LocalGasStationIcon from "@mui/icons-material/LocalGasStation";
+import AlignCenter from "../../view/atoms/AlignCenter";
+import LocalShippingIcon from "@mui/icons-material/LocalShipping";
+import OilBarrelIcon from "@mui/icons-material/OilBarrel";
 
 export default function FuelView({
   label,
@@ -18,15 +22,38 @@ export default function FuelView({
   }
 
   return (
-    <Paper sx={{ m: 1, p: 1 }}>
-      <Typography variant="subtitle1">{label}</Typography>
-      <LabelledBox label="Opening Stock">
-        <FuelQuantity quantity={capacity} color="orange" />
-      </LabelledBox>
+    <Paper sx={{ m: 1, p: 1, width: "80%" }}>
+      <AlignCenter>
+        <LocalGasStationIcon sx={{ color }} />
+        <Typography variant="subtitle1">{label}</Typography>
+      </AlignCenter>
+      {capacity ? (
+        <LabelledBox
+          label={
+            <AlignCenter>
+              Opening Stock
+              <OilBarrelIcon
+                sx={{ paddingLeft: 0.5, fontSize: "small", color: "orange" }}
+              />
+            </AlignCenter>
+          }
+        >
+          <FuelQuantity quantity={capacity} color="orange" />
+        </LabelledBox>
+      ) : null}
 
       {recentDispatch ? (
-        <LabelledBox label="Recent Dispatch">
-          <FuelQuantity quantity={recentDispatch["amount"]} color="green"/>
+        <LabelledBox
+          label={
+            <AlignCenter>
+              Recent Dispatch
+              <LocalShippingIcon
+                sx={{ paddingLeft: 0.5, fontSize: "small", color: "green" }}
+              />
+            </AlignCenter>
+          }
+        >
+          <FuelQuantity quantity={recentDispatch["amount"]} color="green" />
           <Place name={recentDispatch["plant_name"]} />
           <HumanTime ut={recentDispatch["time_eta_ut"]} />
         </LabelledBox>
