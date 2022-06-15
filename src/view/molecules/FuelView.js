0d17fdf75a4644d6cec06005e292d1729e6e8753
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-15 18:02:42 +0530

    Use new widgets

--- src/view/molecules/FuelView.js
+++ src/view/molecules/FuelView.js
@@ -1,26 +1,37 @@
-import Avatar from "@mui/material/Avatar";
-import ListItem from "@mui/material/ListItem";
-import ListItemAvatar from "@mui/material/ListItemAvatar";
-import ListItemText from "@mui/material/ListItemText";
+import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
-import LocalGasStationIcon from "@mui/icons-material/LocalGasStation";
 
-export default function FuelView({ label, availlability, capacity, color }) {
+import FuelQuantity from "../../view/atoms/FuelQuantity";
+import HumanTime from "../../view/atoms/HumanTime";
+import Place from "../../view/atoms/Place";
+import LabelledBox from "../../view/molecules/LabelledBox";
+
+export default function FuelView({
+  label,
+  availlability,
+  capacity,
+  recentDispatch,
+  color,
+}) {
   if (!availlability) {
     return null;
   }
+
   return (
-    <ListItem sx={{ p: 0, m: 0, maxHeight: "8vh" }}>
-      <ListItemAvatar>
-        <Avatar sx={{ color, backgroundColor: "#f8f8f8" }}>
-          <LocalGasStationIcon />
-        </Avatar>
-      </ListItemAvatar>
-      <ListItemText
-        primary={<Typography sx={{ fontSize: "small" }}>{label}</Typography>}
-        sx={{ color }}
-      />
-    </ListItem>
+    <Paper sx={{ m: 1, p: 1 }}>
+      <Typography variant="subtitle1">{label}</Typography>
+      <LabelledBox label="Opening Stock">
+        <FuelQuantity quantity={capacity} />
+      </LabelledBox>
+
+      {recentDispatch ? (
+        <LabelledBox label="Recent Dispatch">
+          <FuelQuantity quantity={recentDispatch["amount"]} />
+          <Place name={recentDispatch["plant_name"]} />
+          <HumanTime ut={recentDispatch["time_eta_ut"]} />
+        </LabelledBox>
+      ) : null}
+    </Paper>
   );
 }
 
