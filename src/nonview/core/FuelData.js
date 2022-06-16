7019c45ab9c7b21a278aaaad00b51a8813d5c1d6
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-16 15:32:15 +0530

    Added jsonCache

--- src/nonview/core/FuelData.js
+++ src/nonview/core/FuelData.js
@@ -8,14 +8,28 @@ const JSON_HEADERS = {
 
 async function jsonNonCache(url) {
   const response = await fetch(url, JSON_HEADERS);
-  const dataJson = await response.json();
-  return dataJson;
+  const data = await response.json();
+  return data;
+}
+
+async function jsonCache(url) {
+  const cacheKey = "cache-" + url;
+  const dataJSON = localStorage.getItem(cacheKey);
+  if (dataJSON && dataJSON !== "" && dataJSON !== "null") {
+    const data = JSON.parse(dataJSON);
+    return data;
+  }
+
+  const dataHot = await jsonNonCache(url);
+  const dataJSONHot = JSON.stringify(dataHot);
+  localStorage.setItem(cacheKey, dataJSONHot);
+  return dataHot;
 }
 
 export default class FuelData {
   static async multigetExtendedShedList() {
     const url = URL_DATA_ROOT + "/latest/extended_shed_list.json";
-    const rawDataList = await jsonNonCache(url);
+    const rawDataList = await jsonCache(url);
     return rawDataList.sort(function (a, b) {
       const ta = a["time_last_updated_by_shed_ut"];
       const tb = b["time_last_updated_by_shed_ut"];
