ed42635a38f2b0565bc6dd2f9cb9ebf4fa88e9f9
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-16 18:11:06 +0530

    Added Filter Menu etc

--- src/view/molecules/CustomAppBar.js
+++ src/view/molecules/CustomAppBar.js
@@ -1,25 +1,29 @@
 import React from "react";
 
 import Box from "@mui/material/Box";
+import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
+import FilterDetails from "../../view/atoms/FilterDetails";
 
 import SettingsMenu from "./SettingsMenu.js";
+import FilterMenu from "./FilterMenu.js";
 
-const STYLE = {
-  position: "fixed",
-  top: 0,
-  left: 0,
-  right: 0,
-  height: 50,
-  zIndex: 1000,
-};
-
-export default function CustomAppBar() {
+export default function CustomAppBar({
+  onSelectFuelTypeList,
+  selectedFuelTypeList,
+}) {
   return (
-    <Box sx={STYLE}>
-      <Toolbar variant="dense">
-        <SettingsMenu />
-      </Toolbar>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" color="transparent">
+        <Toolbar>
+          <SettingsMenu />
+          <FilterMenu
+            onSelectFuelTypeList={onSelectFuelTypeList}
+            selectedFuelTypeList={selectedFuelTypeList}
+          />
+          <FilterDetails selectedFuelTypeList={selectedFuelTypeList} />
+        </Toolbar>
+      </AppBar>
     </Box>
   );
 }
