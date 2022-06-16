5957a7cd922029ef01e68d03dafedfdc78bcda0a
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-16 18:26:42 +0530

    Hide All Fuels / Added Color

--- src/view/atoms/FilterDetails.js
+++ src/view/atoms/FilterDetails.js
@@ -1,14 +1,18 @@
 import Typography from "@mui/material/Typography";
+
 import Fuel from "../../nonview/core/Fuel";
 
 const STYLE_TEXT = {
-  background: "rgba(255, 255, 255, 0.8)",
   borderRadius: 6,
   padding: 0.5,
+  color: "orange",
 };
 
 export default function FilterDetails({ selectedFuelTypeList }) {
   const label = Fuel.getFuelTypeGroupLabel(selectedFuelTypeList);
+  if (label === "All Fuels") {
+    return null;
+  }
   return (
     <Typography variant="caption" sx={STYLE_TEXT}>
       {label}
