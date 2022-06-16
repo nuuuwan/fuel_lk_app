c2e8aa2ff18fcd1da93752936dae6df3eff67799
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-16 21:21:28 +0530

    Title

--- src/view/molecules/CustomAppBar.js
+++ src/view/molecules/CustomAppBar.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+import Typography from '@mui/material/Typography';
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
@@ -20,6 +21,12 @@ export default function CustomAppBar({
             onSelectFuelTypeList={onSelectFuelTypeList}
             selectedFuelTypeList={selectedFuelTypeList}
           />
+          <Typography variant="body1" sx={{flexGrow: 1}}>
+            {" "}
+          </Typography>
+          <Typography variant="body1" sx={{color: "black"}}>
+            #FuelLKApp
+          </Typography>
         </Toolbar>
       </AppBar>
     </Box>
