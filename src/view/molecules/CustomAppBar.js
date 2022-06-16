70016405179b07ac5ac4fede391e3c0ab01b8bf1
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-16 21:40:09 +0530

    Use Logo Avatar; Changed title

--- src/view/molecules/CustomAppBar.js
+++ src/view/molecules/CustomAppBar.js
@@ -5,6 +5,7 @@ import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 
+import LogoAvatar from "../../view/atoms/LogoAvatar";
 import FilterMenu from "./FilterMenu.js";
 import HelpMenu from "./HelpMenu.js";
 
@@ -16,8 +17,9 @@ export default function CustomAppBar({
     <Box>
       <AppBar position="static" style={{ background: "white" }}>
         <Toolbar>
-          <Typography variant="body1" sx={{ color: "black" }}>
-            #FuelLKApp
+          <LogoAvatar />
+          <Typography variant="subtitle1" sx={{ color: "black" }}>
+            Fuel in Sri Lanka
           </Typography>
           <Typography variant="body1" sx={{ flexGrow: 1 }}>
             {" "}
