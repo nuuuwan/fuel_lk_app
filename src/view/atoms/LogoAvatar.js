68167447403c1c8474fc2a210bc1bfdac32ea25d
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-16 21:39:57 +0530

    Added LogoAvatar

---
+++ src/view/atoms/LogoAvatar.js
@@ -0,0 +1,12 @@
+import Avatar from "@mui/material/Avatar";
+
+const DIM = 24;
+export default function LogoAvatar() {
+  return (
+    <Avatar
+      alt={"logo"}
+      src={process.env.PUBLIC_URL + "/" + "logo192.png"}
+      sx={{ width: DIM, height: DIM }}
+    />
+  );
+}
