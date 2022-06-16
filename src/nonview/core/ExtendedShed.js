05d55dde9462e4977670c036ee08e2185e7ec595
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-16 10:57:42 +0530

    Added ExtendedShed with various utils

---
+++ src/nonview/core/ExtendedShed.js
@@ -0,0 +1,24 @@
+import TimeX, { SECONDS_IN } from "../../nonview/base/TimeX";
+import { MAX_RECENCY_HOURS } from "../../nonview/constants/Constants.js";
+
+export default class ExtendedShed {
+  static getHasRecentDispatch(extendedShed) {
+    const currentTime = TimeX.getUnixTime();
+    for (let dispatch of extendedShed["dispatch_schedule_list"]) {
+      const deltaToDispatch = dispatch["time_eta_ut"] - currentTime;
+      if (deltaToDispatch > -MAX_RECENCY_HOURS * SECONDS_IN.HOUR) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  static getHasListedStock(extendedShed) {
+    for (let value of Object.values(extendedShed["fuel_status_idx"])) {
+      if (value.capacity > 0) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
