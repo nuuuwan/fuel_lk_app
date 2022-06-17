c8007914b7553b415ccd01eec6c77e9b112352f1
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-17 21:39:18 +0530

    Added get and set Lang

--- src/nonview/base/I18N.js
+++ src/nonview/base/I18N.js
@@ -1,17 +1,35 @@
 import DICTIONARY from "../../nonview/base/DICTIONARY";
 
 export const BASE_LANG = "en";
-
+const CACHE_KEY_LANG = "CACHE_KEY_LANG";
+export const LANG_LIST = ["en", "si", "ta"];
 export default class I18N {
   static getCurrentLang() {
-    return "si";
+    let browserLang = localStorage.getItem(CACHE_KEY_LANG);
+    if (!browserLang) {
+      browserLang = "en";
+      localStorage.setItem(CACHE_KEY_LANG, browserLang);
+    }
+    return browserLang;
+  }
+
+  static setCurrentLang(browserLang) {
+    localStorage.setItem(CACHE_KEY_LANG, browserLang);
+  }
+
+  static getLangName(lang) {
+    return {
+      en: "English",
+      si: "සිංහල",
+      ta: "தமிழ்",
+    }[lang];
   }
 
   static translate(s) {
-    if (!s || !s.trim()) {
+    const currentLang = I18N.getCurrentLang();
+    if (!s || !s.trim() || currentLang === BASE_LANG) {
       return s;
     }
-    const currentLang = I18N.getCurrentLang();
     const entry = DICTIONARY[s];
     if (!entry) {
       console.warn(`[I18N] ${s}`);
