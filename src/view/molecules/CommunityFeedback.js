a1f4e89eff70fcaea8148c7edff6854287ff01bd
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-17 17:29:24 +0530

    Mostly formatting changes

--- src/view/molecules/CommunityFeedback.js
+++ src/view/molecules/CommunityFeedback.js
@@ -4,6 +4,8 @@ import AlignCenter from "../../view/atoms/AlignCenter";
 import LabelledBox from "../../view/molecules/LabelledBox";
 import PollOptionView from "../../view/molecules/PollOptionView";
 
+const MIN_VALID_MESSAGE_COUNT = 1;
+
 export default function CommunityFeedback({ summaryFeedback }) {
   if (!summaryFeedback) {
     return null;
@@ -14,13 +16,16 @@ export default function CommunityFeedback({ summaryFeedback }) {
     return null;
   }
 
-  const topPollOptionID = sortedPollOptionIDAndCountPairs[0][0];
+  const [topPollOptionID, topCount] = sortedPollOptionIDAndCountPairs[0];
+  const topP = topCount / validMessageCount;
+  const isSufficient =
+    topP > 0.5 && validMessageCount >= MIN_VALID_MESSAGE_COUNT;
 
   return (
     <LabelledBox
       label={
         <AlignCenter>
-          {`Community Feedback (${validMessageCount})`}
+          {`Recent Community Feedback (${validMessageCount})`}
           <PeopleIcon
             sx={{
               paddingLeft: 0.5,
@@ -31,7 +36,11 @@ export default function CommunityFeedback({ summaryFeedback }) {
         </AlignCenter>
       }
     >
-      <PollOptionView pollOptionID={topPollOptionID} />
+      {isSufficient ? (
+        <PollOptionView pollOptionID={topPollOptionID} />
+      ) : (
+        "Need more feedback"
+      )}
     </LabelledBox>
   );
 }
