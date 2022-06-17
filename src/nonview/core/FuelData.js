17aa6e4c9d5986fa3d61e5ac91eb016938ff34a8
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-17 15:35:38 +0530

    Use CacheX

--- src/nonview/core/FuelData.js
+++ src/nonview/core/FuelData.js
@@ -1,30 +1,6 @@
+import { jsonCache } from "../../nonview/base/CacheX";
+
 const URL_DATA_ROOT = "https://raw.githubusercontent.com/nuuuwan/fuel_lk/data";
-
-const JSON_HEADERS = {
-  headers: {
-    Accept: "application/json",
-  },
-};
-
-async function jsonNonCache(url) {
-  const response = await fetch(url, JSON_HEADERS);
-  const data = await response.json();
-  return data;
-}
-
-async function jsonCache(url) {
-  const cacheKey = "cache-" + url;
-  const dataJSON = localStorage.getItem(cacheKey);
-  if (dataJSON && dataJSON !== "" && dataJSON !== "null") {
-    const data = JSON.parse(dataJSON);
-    return data;
-  }
-
-  const dataHot = await jsonNonCache(url);
-  const dataJSONHot = JSON.stringify(dataHot);
-  localStorage.setItem(cacheKey, dataJSONHot);
-  return dataHot;
-}
 
 export default class FuelData {
   static async multigetExtendedShedList() {
