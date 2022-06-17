c866cf02c4d2cadd03ecfc371a3080db7227b320
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-17 06:53:15 +0530

    Improved toTitleCase

--- src/nonview/base/StringX.js
+++ src/nonview/base/StringX.js
@@ -1,5 +1,6 @@
 export default class StringX {
   static toTitleCase(s) {
+    s = s.replaceAll('.', '. ').replaceAll('  ', ' ');
     return s.replace(/\w\S*/g, function (s1) {
       return s1.charAt(0).toUpperCase() + s1.substr(1).toLowerCase();
     });
