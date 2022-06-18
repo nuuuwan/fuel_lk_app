affe67024ca8aa44a0dd48e5a816f5493e2068ec
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-18 07:40:13 +0530

    Use JSONWWW

--- src/nonview/core/FuelData.js
+++ src/nonview/core/FuelData.js
@@ -1,11 +1,13 @@
-import { jsonCache } from "../../nonview/base/CacheX";
+import { JSONWWW } from "../../nonview/base/WWW";
 
 const URL_DATA_ROOT = "https://raw.githubusercontent.com/nuuuwan/fuel_lk/data";
 
 export default class FuelData {
   static async multigetExtendedShedList() {
-    const url = URL_DATA_ROOT + "/latest/extended_shed_list.json";
-    const rawDataList = await jsonCache(url);
+    const rawDataList = await new JSONWWW(
+      URL_DATA_ROOT + "/latest/extended_shed_list.json"
+    ).read();
+
     return rawDataList.sort(function (a, b) {
       const ta = a["time_last_updated_by_shed_ut"];
       const tb = b["time_last_updated_by_shed_ut"];
