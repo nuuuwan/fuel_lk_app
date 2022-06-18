bf163419290d470ec48ab7fe7c08905231a7b252
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-18 12:26:03 +0530

    Lint

--- src/view/molecules/TempSourceIsDown.js
+++ src/view/molecules/TempSourceIsDown.js
@@ -15,11 +15,8 @@ export default function TempSourceIsDown() {
     >
       <Alert severity="error">
         <Link href="https://fuel.gov.lk/">{"https://fuel.gov.lk/ "}</Link>
-        has been temporary shut down.
-        Since this App sources data from
-        fuel.gov.lk,
-        dynamic data will be out of date.
-        Sorry ☹️.
+        has been temporary shut down. Since this App sources data from
+        fuel.gov.lk, dynamic data will be out of date. Sorry ☹️.
       </Alert>
     </Box>
   );
