ff9d968fc99b9b95704c604e66e9c8bc1ff3f36b
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-18 12:25:22 +0530

    Added FilterMenuGeneric

---
+++ src/view/molecules/FilterMenuGeneric.js
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+
+import IconButton from "@mui/material/IconButton";
+import ListItemText from "@mui/material/ListItemText";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import Tooltip from "@mui/material/Tooltip";
+import Typography from "@mui/material/Typography";
+
+import { t } from "../../nonview/base/I18N";
+
+export default function FilterMenuGeneric({
+  onSelect,
+  selectedOptionID,
+  defaultOptionID,
+  optionIdx,
+  Icon,
+  colorSelected,
+}) {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+
+  const selectionOption = optionIdx[selectedOptionID];
+  const isSelectedDefault = selectedOptionID === defaultOptionID;
+  const colorIcon = isSelectedDefault ? "neutral" : colorSelected;
+
+  const onClick = function (e) {
+    setAnchorEl(e.currentTarget);
+  };
+
+  const onClose = function () {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div>
+      <IconButton size="large" onClick={onClick} sx={{ p: 1 }}>
+        <Typography variant="caption" sx={{ padding: 0.25, color: colorIcon }}>
+          {t(selectionOption.label)}
+        </Typography>
+        <Tooltip title={t("Filter by Time Updated")}>
+          <Icon sx={{ color: colorIcon }} />
+        </Tooltip>
+      </IconButton>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={onClose}
+        MenuListProps={{
+          "aria-labelledby": "basic-button",
+        }}
+      >
+        {Object.entries(optionIdx).map(function ([optionID, option], iOption) {
+          const key = "option-" + iOption;
+          const onClickInner = function () {
+            onClose();
+            onSelect(optionID);
+          };
+          const isSelected = selectedOptionID === optionID;
+          const color = isSelected ? colorSelected : "neutral";
+          return (
+            <MenuItem key={key} onClick={onClickInner}>
+              <ListItemText sx={{ color }}>{t(option.label)}</ListItemText>
+            </MenuItem>
+          );
+        })}
+      </Menu>
+    </div>
+  );
+}
