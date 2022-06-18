e4a85f584bed04dd6c3c0f01a5f044c4c511bd3c
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-18 12:39:30 +0530

    Added FilterMenuLang

---
+++ src/view/molecules/FilterMenuLang.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import { useTheme } from "@mui/material/styles";
+
+import { LANG_IDX } from "../../nonview/base/I18N";
+
+import FilterMenuGeneric from "../../view/molecules/FilterMenuGeneric";
+
+export default function FilterMenuLang({ onSelectLang, selectedLang }) {
+  const theme = useTheme();
+
+  return (
+    <FilterMenuGeneric
+      onSelect={onSelectLang}
+      selectedOptionID={selectedLang}
+      defaultOptionID={""}
+      optionIdx={LANG_IDX}
+      Icon={null}
+      colorSelected={theme.palette.primary.main}
+    />
+  );
+}
