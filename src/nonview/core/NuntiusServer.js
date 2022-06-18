b07ed28ab3c7463ddc8a6c1ed038827502c2efa6
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-18 15:43:43 +0530

    Fixed Nuntius caching issue

--- src/nonview/core/NuntiusServer.js
+++ src/nonview/core/NuntiusServer.js
@@ -1,4 +1,5 @@
 import { JSONWWW } from "../../nonview/base/WWW";
+import Cache from "../../nonview/base/Cache"
 
 export default class NuntiusServer {
   static getURLLambda() {
@@ -13,7 +14,7 @@ export default class NuntiusServer {
       NuntiusServer.getURLLambda() +
         `?payload_json_base64=${payloadJSONB64Encoded}`
     );
-    const response = await jsonWWW.read();
+    const response = await jsonWWW.readNoCache();
     if (response["exception"]) {
       console.error(response);
     }
@@ -21,15 +22,32 @@ export default class NuntiusServer {
   }
 
   static async putMessage(message) {
+    const cache = NuntiusServer.multigetMessagesCache(message.destID);
+    cache.clear();
+
     return await NuntiusServer.generic({
       cmd: "put-message",
       message: message,
     });
   }
-  static async multigetMessages(destID) {
+
+  static async multigetMessagesNoCache(destID) {
     return await NuntiusServer.generic({
       cmd: "multiget-messages",
       destID: destID,
     });
+  }
+
+  static multigetMessagesCache(destID) {
+    return new Cache("multigetMessages:" + destID);
+  }
+
+  static async multigetMessages(destID) {
+    const cache = NuntiusServer.multigetMessagesCache(destID);
+    return cache.get(
+      async function() {
+        return await NuntiusServer.multigetMessagesNoCache(destID);
+      }
+    );
   }
 }
