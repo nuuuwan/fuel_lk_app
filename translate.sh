42cf54ba69c2d9345ebfa98e140f8c00fd7a3c8d
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-18 14:32:47 +0530

    Updated to use new i18n

--- translate.sh
+++ translate.sh
@@ -1,2 +1,2 @@
 cd /Users/nuwan.senaratna/Dropbox/_CODING/py/i18n
-python3  build.py "/Users/nuwan.senaratna/Not.Dropbox/_CODING/js_react/fuel_lk_app/src/nonview/base/english.txt" "/Users/nuwan.senaratna/Not.Dropbox/_CODING/js_react/fuel_lk_app/src/nonview/base/DICTIONARY.js"
+python3  build.py "/Users/nuwan.senaratna/Not.Dropbox/_CODING/js_react/fuel_lk_app/src/nonview/base"
