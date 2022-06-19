d8920b2fabae3b1e0cecaa8e51eebb5dfff28e8b
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-19 11:50:45 +0530

    Added ExtendedShedNew

--- src/nonview/core/ExtendedShed.js
+++ src/nonview/core/ExtendedShed.js
@@ -1,4 +1,59 @@
+import BaseShed, { BASE_SHED_IDX } from "../../nonview/core/BaseShed";
+import DispatchSchedule from "../../nonview/core/DispatchSchedule";
 import FuelGroup from "../../nonview/core/FuelGroup";
+import FuelStatus from "../../nonview/core/FuelStatus";
+
+export class ExtendedShedNew extends BaseShed {
+  constructor(
+    // BaseShed
+    shedID,
+    shedCode,
+    shedName,
+    shedType,
+    address,
+    latLng,
+    gmapAddress,
+    // ExtendedShedNew only
+    timeLastUpdatedByShedUT,
+    dispatchScheduleList,
+    fuelStatusIdx
+  ) {
+    // BaseShed constructor
+    super(shedID, shedCode, shedName, shedType, address, latLng, gmapAddress);
+
+    this.timeLastUpdatedByShedUT = timeLastUpdatedByShedUT;
+    this.dispatchScheduleList = dispatchScheduleList;
+    this.fuelStatusIdx = fuelStatusIdx;
+  }
+
+  static fromDict(d) {
+    const shedCode = d["shed_code"];
+    const baseShed = BASE_SHED_IDX[shedCode];
+    return new ExtendedShed(
+      // BaseShed
+      baseShed.shedID,
+      baseShed.shedCode,
+      baseShed.shedName,
+      baseShed.shedType,
+      baseShed.address,
+      baseShed.latLng,
+      baseShed.gmapAddress,
+      // ExtendedShed Only
+      d["time_last_updated_by_shed_ut"],
+      d["dispatch_schedule_list"].map(function (d1) {
+        return DispatchSchedule.fromDict(d1);
+      }),
+      Object.entries(d["fuel_status_idx"])
+        .map(function (fuelType, d1) {
+          return [fuelType, FuelStatus.fromDict(d1)];
+        })
+        .reduce(function (fuelStatusIdx, [fuelType, fuelStatus]) {
+          fuelStatusIdx[fuelType] = fuelStatus;
+          return fuelStatusIdx;
+        }, {})
+    );
+  }
+}
 
 export default class ExtendedShed {
   static getLastDispatchTime(extendedShed, fuelGroupID) {
