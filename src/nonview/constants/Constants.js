61f14cb6c4723d8ed5bc4da7a9943ac9e7a0a229
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-19 14:20:03 +0530

    Updated DATE_LAST_UPDATE

--- src/nonview/constants/Constants.js
+++ src/nonview/constants/Constants.js
@@ -1,4 +1,4 @@
-export const DATE_LAST_UPDATE = "2022-05-18";
+export const DATE_LAST_UPDATE = "2022-06-19";
 export const MAX_RECENCY_HOURS = 24;
 
 export const URL_APP = "https://nuuuwan.github.io/fuel_lk_app/";
