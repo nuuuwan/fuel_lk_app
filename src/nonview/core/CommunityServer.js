eafce8fa14e3d8bae3f6bb9d012f5d8542b87e98
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-19 14:29:23 +0530

    Added MAX_COMMUNITY_FEEDBACK_AGE_HOURS

--- src/nonview/core/CommunityServer.js
+++ src/nonview/core/CommunityServer.js
@@ -1,8 +1,12 @@
 import NuntiusServer from "../../nonview/base/NuntiusServer";
 import CommunityFeedback from "../../nonview/core/CommunityFeedback";
+import TimeX, { SECONDS_IN } from "../../nonview/base/TimeX";
+
+const MAX_COMMUNITY_FEEDBACK_AGE_HOURS = 1;
 
 export default class CommunityServer {
   static async getCommunityFeedbackIdx() {
+    const currentTimeUT = TimeX.getUnixTime();
     const recentMessageList = await NuntiusServer.multigetRecent();
     const sortedRecentMessageList = recentMessageList.sort(function (a, b) {
       return a.messageTimeUT - b.messageTimeUT;
@@ -11,6 +15,18 @@ export default class CommunityServer {
     let communityFeedbackIdx = {};
 
     for (let message of sortedRecentMessageList) {
+      const timeFeedbackUT = message.messageTimeUT;
+
+      if (!timeFeedbackUT) {
+        console.error("Missing message.messageTimeUT");
+        continue;
+      }
+
+      const messageAge = currentTimeUT - timeFeedbackUT;
+      if (messageAge > MAX_COMMUNITY_FEEDBACK_AGE_HOURS * SECONDS_IN.HOUR) {
+        continue;
+      }
+
       const tokens = message.destID.split(":");
       if (tokens.length !== 3) {
         console.error("Invalid message.destID: " + message.destID);
@@ -33,7 +49,7 @@ export default class CommunityServer {
         shedCode,
         fuelType,
         userID,
-        message.messageTimeUT, // timeFeedbackUT
+        timeFeedbackUT, // timeFeedbackUT
         message.messageText // pollOptionID
       );
     }
