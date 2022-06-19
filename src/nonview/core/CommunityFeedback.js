987b0da5f3156d711f1fa6b2fac7ed84cdcf3484
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-19 13:44:05 +0530

    Added CommunityFeedback

---
+++ src/nonview/core/CommunityFeedback.js
@@ -0,0 +1,9 @@
+export default class CommunityFeedback {
+  constructor(shedCode, fuelType, userID, timeFeedbackUT, pollOptionID) {
+    this.shedCode = shedCode;
+    this.fuelType = fuelType;
+    this.userID = userID;
+    this.timeFeedbackUT = timeFeedbackUT;
+    this.pollOptionID = pollOptionID;
+  }
+}
