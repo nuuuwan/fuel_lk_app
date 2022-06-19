fcd136f52a7469c470830759dca9d968614eb19f
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-19 14:21:40 +0530

    Lint

--- src/nonview/core/PollWaitingTime.js
+++ src/nonview/core/PollWaitingTime.js
@@ -6,8 +6,8 @@ import SentimentVeryDissatisfiedIcon from "@mui/icons-material/SentimentVeryDiss
 
 import GhostUser from "../../nonview/base/GhostUser";
 import ID from "../../nonview/base/ID";
-import TimeX, { SECONDS_IN } from "../../nonview/base/TimeX";
 import NuntiusServer from "../../nonview/base/NuntiusServer";
+import TimeX, { SECONDS_IN } from "../../nonview/base/TimeX";
 
 export const POLL_OPTION_IDX = {
   lessThan30Mins: {
