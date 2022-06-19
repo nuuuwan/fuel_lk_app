ef9cafc8dd981dec0832f3e8eada994671a89bcd
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-19 17:01:31 +0530

    Use ShedBlurb

--- src/view/molecules/ShedDrawer.js
+++ src/view/molecules/ShedDrawer.js
@@ -1,17 +1,15 @@
 import Box from "@mui/material/Box";
 import Drawer from "@mui/material/Drawer";
 import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
 import CloseIcon from "@mui/icons-material/Close";
 
 import { t } from "../../nonview/base/I18N";
 
 import AlignCenter from "../../view/atoms/AlignCenter";
 import HumanTime from "../../view/atoms/HumanTime";
-import Link from "../../view/atoms/Link";
-import ShedAvatar from "../../view/atoms/ShedAvatar";
 import FuelsView from "../../view/molecules/FuelsView";
 import LabelledBox from "../../view/molecules/LabelledBox";
+import ShedBlurb from "../../view/molecules/ShedBlurb";
 
 const STYLE = { m: 1, p: 1, width: 250 };
 
@@ -24,21 +22,13 @@ export default function ShedDrawer({ extendedShed, onCloseDrawer }) {
     <Drawer anchor={"right"} open={true} onClose={onCloseDrawer}>
       <Box sx={STYLE}>
         <AlignCenter>
-          <ShedAvatar extendedShed={extendedShed} />
-          <Typography variant="subtitle2">
-            {t(extendedShed.shedName)}
-          </Typography>
+          <ShedBlurb extendedShed={extendedShed} />
           <Box display="flex" justifyContent="flex-end">
             <IconButton onClick={onCloseDrawer}>
               <CloseIcon />
             </IconButton>
           </Box>
         </AlignCenter>
-        <Link href={extendedShed.gmapsURL}>
-          <Typography variant="caption">
-            {t(extendedShed.gmapsAddress)}
-          </Typography>
-        </Link>
         <FuelsView extendedShed={extendedShed} />
         <LabelledBox label={t("When was the data updated?")}>
           <HumanTime ut={extendedShed.timeLastUpdatedByShedUT} />
