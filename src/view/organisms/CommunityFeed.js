35d036dc55c153806b7f5f1d6bada96f8586a683
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-19 16:10:41 +0530

    CommunityFeed Shell

---
+++ src/view/organisms/CommunityFeed.js
@@ -0,0 +1,52 @@
+import { Component } from "react";
+import Drawer from "@mui/material/Drawer";
+import Box from "@mui/material/Box";
+import CommunityServer from "../../nonview/core/CommunityServer";
+import CircularProgress from "@mui/material/CircularProgress";
+
+const STYLE = { m: 1, p: 1, width: 250 };
+
+export default class CommunityFeed extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { communityFeedbackIdx: undefined };
+  }
+
+  async componentDidMount() {
+    this.setState({
+      communityFeedbackIdx: await CommunityServer.getCommunityFeedbackIdx(),
+    });
+  }
+
+  renderInner() {
+    const { communityFeedbackIdx } = this.state;
+    if (!communityFeedbackIdx) {
+      return <CircularProgress />;
+    }
+
+    return Object.entries(communityFeedbackIdx).map(function ([
+      shedCode,
+      idxForShed,
+    ]) {
+      return Object.entries(idxForShed).map(function ([
+        fuelType,
+        idxForFuelType,
+      ]) {
+        return Object.values(idxForFuelType).map(function (communityFeedback) {
+          return JSON.stringify(communityFeedback);
+        });
+      });
+    });
+  }
+
+  render() {
+    const { isOpenFeed, onCloseDrawerFeed } = this.props;
+    return (
+      <Drawer anchor={"right"} open={isOpenFeed} onClose={onCloseDrawerFeed}>
+        <Box sx={STYLE}>{this.renderInner()}</Box>
+      </Drawer>
+    );
+  }
+}
+
+// import CommunityFeed from "../../view/organisms/CommunityFeed";
