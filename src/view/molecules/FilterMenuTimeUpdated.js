00885feb1206efc1dc9c5ac4eff53a9ec3716232
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-19 14:21:58 +0530

    Added time options

--- src/view/molecules/FilterMenuTimeUpdated.js
+++ src/view/molecules/FilterMenuTimeUpdated.js
@@ -3,6 +3,8 @@ import React from "react";
 import { useTheme } from "@mui/material/styles";
 import AccessTimeIcon from "@mui/icons-material/AccessTime";
 
+import { HOURS_IN } from "../../nonview/base/TimeX";
+
 import FilterMenuGeneric from "../../view/molecules/FilterMenuGeneric";
 
 const TIME_UPDATED_OPTION_IDX = {
@@ -10,7 +12,8 @@ const TIME_UPDATED_OPTION_IDX = {
   3: { label: "Last 3 Hours" },
   6: { label: "Last 6 Hours" },
   12: { label: "Last 12 Hours" },
-  24: { label: "Last 24 Hours" },
+  [HOURS_IN.DAY]: { label: "Last 24 Hours" },
+  [HOURS_IN.WEEK]: { label: "Last Week" },
 };
 
 export default function FilterMenuTimeUpdated({
